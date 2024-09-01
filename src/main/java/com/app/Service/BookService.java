4e9b7ec9a2432f2f10ee24e733dd4f8bfaa1046f
pArmarpaRth22 <parmarparth10945@gmail.com>
2024-09-01 12:16:38 +0530

    implemented addbook so that it pass the test case

--- src/main/java/com/app/Service/BookService.java
+++ src/main/java/com/app/Service/BookService.java
@@ -1,4 +1,25 @@
 package main.java.com.app.Service;
 
+import main.java.com.app.DAO.BookDAO;
+import main.java.com.app.exception.BookNotAvailableException;
+import main.java.com.app.model.Book;
 
-public class BookService {}
+import java.util.Date;
+import java.util.List;
+
+public class BookService {
+    private final BookDAO bookDAO;
+
+
+    public BookService(BookDAO bookDAO) {
+        this.bookDAO = bookDAO;
+    }
+
+    public void addBook(String ISBN,String title,String author,int publicationYear){
+
+        Book book=new Book(ISBN,title,author,publicationYear);
+        bookDAO.addBook(book);
+    }
+
+
+}
