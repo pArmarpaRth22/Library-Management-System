0bb9a3d704fc59ce9586fe3fe44a0919220dab21
pArmarpaRth22 <parmarparth10945@gmail.com>
2024-09-01 12:56:47 +0530

    added BookNotAvailable and EmptyField custom exception

---
+++ src/main/java/com/app/exception/BookNotAvailableException.java
@@ -0,0 +1,8 @@
+package main.java.com.app.exception;
+
+public class BookNotAvailableException extends Exception{
+
+    public BookNotAvailableException(String message){
+        super(message);
+    }
+}
---
+++ src/main/java/com/app/exception/EmptyFieldException.java
@@ -0,0 +1,7 @@
+package main.java.com.app.exception;
+
+public class EmptyFieldException extends Exception{
+    public EmptyFieldException(String message){
+        super(message);
+    }
+}
