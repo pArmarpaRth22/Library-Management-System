f847eb1fdd42df439839f14a54920eaee885b9a9
pArmarpaRth22 <parmarparth10945@gmail.com>
2024-09-01 11:52:11 +0530

    implemented BookDAO interface

---
+++ src/main/java/com/app/DAO/BookDAOImp.java
@@ -0,0 +1,46 @@
+package main.java.com.app.DAO;
+
+import main.java.com.app.model.Book;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookDAOImp  implements BookDAO{
+
+    private List<Book> books=new ArrayList<>();
+    @Override
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    @Override
+    public Book getBookByISBN(String ISBN) {
+        return books.stream()
+                .filter(book->book.getISBN().equals(ISBN))
+                .findFirst()
+                .orElse(null);
+    }
+
+    @Override
+    public List<Book> getAllBooks() {
+        return books;
+    }
+
+    @Override
+    public void updateBook(Book book) {
+        Book existingBook=getBookByISBN(book.getISBN());
+        if(existingBook!=null){
+            existingBook.setAuthor(book.getAuthor());
+            existingBook.setTitle(book.getTitle());
+            existingBook.setPublicationYear(book.getPublicationYear());
+            existingBook.setAvailable(book.isAvailable());
+
+        }
+
+    }
+
+    @Override
+    public void deleteBook(String ISBN) {
+        books.removeIf(book->book.getISBN().equals(ISBN));
+    }
+}
